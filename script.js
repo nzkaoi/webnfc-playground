51ecf74b41bde9d53539e6c4a38d528c41f9ca02
nzkaoi <nzkaoi@gmail.com>
2024-01-28 12:33:04 +0900

    Update script.js
    delete write button

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
 let inputText = document.getElementById("writeText");
-const writeButton = document.getElementById("writeButton");
-const writeLog = document.getElementById("writeLog");
-
 const readButton = document.getElementById("readButton");
 const readLog = document.getElementById("readLog");
-
-writeButton.addEventListener("click", async () => {
-  console.log("write button clicked");
-  writeLog.textContent = await "please touch the NFC tag!";
-  try {
-    let writeText = inputText.value
-    console.log(writeText)
-    if (!writeText) {
-      writeLog.textContent = "empty";
-      return;
-    }
-    const reader = new NDEFReader();
-    await reader.write(writeText);
-    writeLog.textContent = `write: ${writeText}`;
-  } catch (error) {
-    writeLog.textContent = error;
-    console.log(error);
-  }
-});
 
 readButton.addEventListener("click", async () => {
   readLog.textContent = await "clicked read button";
